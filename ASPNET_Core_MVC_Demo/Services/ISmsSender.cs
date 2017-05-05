de7a8dcb2f30b5b9d9edcfc50a765f15b00f9d82
JayLiu <liuyf115@qq.com>
2017-05-05 09:52:04 +0800

    Create project

---
+++ ASPNET_Core_MVC_Demo/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNET_Core_MVC_Demo.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
